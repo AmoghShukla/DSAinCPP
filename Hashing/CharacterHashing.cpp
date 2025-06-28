7f2fdf2411c7b656f5606eecf538eef70fbb3942
Amogh Shukla <amoghshukla548@gmail.com>
2025-06-28 23:25:44 +0530

    Hashing/Character Hashing Modified

--- Hashing/CharacterHashing.cpp
+++ Hashing/CharacterHashing.cpp
@@ -23,3 +23,29 @@ int main() {
         cout << hash[s1-'a'] << endl;
     }
 }
+
+// In case its not mentioned wether the string is lowercase or uppercase, you can modify the hash array to accommodate both cases:
+/*
+int main() {
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+
+    // hash compute
+    int hash[256] = {0};
+    for(int i=0; i<s.size(); i++){
+        hash[s[i]]++;
+    }
+
+    int n;
+    cout << "Enter the number of characters to check: ";
+    cin >> n;
+    while(n--){
+        char s1;
+        cout << "Enter the character to check its frequency: ";
+        cin >> s1;
+        // fetch
+        cout << hash[s1] << endl;
+    }
+}
+*/
