11e685b2481a7d45b9293a98ba135d5fae78721c
Anjali Shukla <amoghshukla548@gmail.com>
2025-07-27 22:20:22 +0530

    Array Updated

--- Arrays/Binary_Search/Minimum_In_Rotated_Sorted_Array.cpp
+++ Arrays/Binary_Search/Minimum_In_Rotated_Sorted_Array.cpp
@@ -34,7 +34,7 @@ int main() {
     cin >> n;
     vector<int> arr(n);
     for(int i = 0; i < n; i++) {
-        cout << "Enter element " << i + 1 << ": ";
+        cout << "Enter the element " << i + 1 << ": ";
         cin >> arr[i];
     }
     
